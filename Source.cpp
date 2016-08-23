92fc557426010f22500a1d5e3e4fc2b4d8114f3e
Conner-Stack <connerstack@gmail.com>
2016-08-23 14:11:37 -0500

    changes
    changes to exercises.

--- Source.cpp
+++ Source.cpp
@@ -6,93 +6,102 @@ int main()
 {
 	//EXERCISE 1
 
-	/*int y = 0;
-	int x;
+	//int y = 0;
+	//int x;
 
-	cin >> y;
-	if (y == 0)
-	{
-		x = 100;
-	}
-	cout << x << endl;
-	system("pause");
-	return 0;
-*/
-
-	//EXERCISE 2
-	//int A;
-	//int B;
-
-	//cout << "input 2 numbers"<<endl;
-	//cin >> A >> B;
-
-	//if (A > B)
+	//cin >> y;
+	//if (y == 0)
 	//{
-	//	cout << A << endl;
+	//	x = 100;
 	//}
-	//else if (B > A)
-	//{
-	//	cout << B << endl;
-	//}
-	//else
-	//{
-	//	cout << "you had 1 job >:C" << endl;
-	//}
+	//cout << x << endl;
 	//system("pause");
 	//return 0;
 
+
+	//EXERCISE 2
+	/*int A;
+	int B;
+
+	cout << "input 2 numbers"<<endl;
+	cin >> A >> B;
+
+	if (A > B)
+	{
+		cout << A << endl;
+	}
+	else if (B > A)
+	{
+		cout << B << endl;
+	}
+	else
+	{
+		cout << "you had 1 job >:C" << endl;
+	}
+	system("pause");
+	return 0;*/
+
 	//EXAMPLE 3
 	
-	int numbers[3];
+	/*int numbers[3];
 	int sum = 0;
 
 	cin >> numbers[0] >> numbers[1] >> numbers[2];
 
 	for (int i = 0; i < 3; i++)
 	{
-		
+
 		sum = sum + numbers[i];
 
-			
-	}
-	
-
-	
-	if (sum > 0)
-	{
-		if (numbers[0] < numbers[1] < numbers[2])
-			cout << numbers[0] << numbers[1] << numbers[2];
-		else if (numbers[0] < numbers[2] < numbers[1])
-			cout << numbers[0] << numbers[2] << numbers[1];
-		else if (numbers[1] < numbers[2] < numbers[0])
-			cout << numbers[1] << numbers[2] << numbers[0];
-		else if (numbers[1] < numbers[0] < numbers[2])
-			cout << numbers[1] << numbers[0] << numbers[2];
-		else if (numbers[2] < numbers[0] < numbers[1])
-			cout << numbers[2] << numbers[0] << numbers[1];
-		else
-			cout << numbers[2] << numbers[1] << numbers[0];
 
 	}
-	else if (sum < 0)
-	{
 
-
-
-	}
-	else
-	{
-		for (int i = 0; i < 3; i++)
+ if (sum > 0)
 		{
-			cout << numbers[i] << endl;
+			if (numbers[0] < numbers[1] && numbers[1] < numbers[2])
+				cout << numbers[0] << numbers[1] << numbers[2];
+			else if (numbers[0] < numbers[2] && numbers[2]< numbers[1])
+				cout << numbers[0] << numbers[2] << numbers[1];
+			else if (numbers[1] < numbers[2] && numbers[2] < numbers[0])
+				cout << numbers[1] << numbers[2] << numbers[0];
+			else if (numbers[1] < numbers[0] && numbers[0] < numbers[2])
+				cout << numbers[1] << numbers[0] << numbers[2];
+			else if (numbers[2] < numbers[0] && numbers[0] < numbers[1])
+				cout << numbers[2] << numbers[0] << numbers[1];
+			else if (numbers[2] <numbers[1] && numbers[1] < numbers[0])
+				cout << numbers[2] << numbers[1] << numbers[0];
+
 		}
+		else if (sum < 0)
+		{
+			if (numbers[0] > numbers[1] && numbers[1] > numbers[2])
+				cout << numbers[0] << numbers[1] << numbers[2];
+			else if (numbers[0] > numbers[2] && numbers[2] > numbers[1])
+				cout << numbers[0] << numbers[2] << numbers[1];
+			else if (numbers[1] > numbers[2] && numbers[2] > numbers[0])
+				cout << numbers[1] << numbers[2] << numbers[0];
+			else if (numbers[1] > numbers[0] && numbers[0] > numbers[2])
+				cout << numbers[1] << numbers[0] << numbers[2];
+			else if (numbers[2] > numbers[0] && numbers[0] > numbers[1])
+				cout << numbers[2] << numbers[0] << numbers[1];
+			else if (numbers[2] > numbers[1] && numbers[1] > numbers[0])
+				cout << numbers[2] << numbers[1] << numbers[0];
+
+		}
+		else
+		{
+			for (int i = 0; i < 3; i++)
+			{
+				cout << numbers[i] << endl;
+			}
+		
 	}
 	system("pause");
-	return 0;
+	return 0;*/
 
-	//EXAMPLE 4
+	/*EXAMPLE 4
 	
-	/*int(choice);
+	int(choice);
 	cin >> choice;
 	switch (choice)
 	{
@@ -104,63 +113,61 @@ int main()
 	case 3:
 		cout << "4" << endl;
 	default:
-		cout << "invalid"<<endl;
-	
-	
-	
-	
-	}
-	system("pause");
+		cout << "invalid"<<endl;*/
+	//}
+	/*system("pause");
 	return 0;*/
 
-	//Example5
-	/*int x;
-	int y;
-	int answer;
-	cin >> x;
-
-	answer = (x == 0) ? y = 0 : y = 10 / x;
-	system("pause");
-	return 0;
-	cout << answer << endl;*/
-	//Example6
-
-	/*int A;
-	int B;
-	char op;
-	int answer;
-	
-	cin >> A >> op >> B;
-	
-	switch (op)
-	{
-	case '+':
-		answer = A + B;
-		cout << answer << endl;
-		break;
-	case '-':
-		answer = A - B;
-		cout << answer << endl;
-		break;
-	case '/':
-		answer = A / B;
-		cout << answer << endl;
-		break;
-	case '*':
-		answer = A * B;
-		cout << answer << endl;
-		break;
-	case '%':
-		answer = A % B;
-		cout << answer << endl;
-		break;
-	default:
-		cout << "invalid input" << endl;
-	}
-	
-	system("pause");
-	return 0;*/
-
+//	Example5
+//	int x;
+//	int y;
+//	int answer;
+//	cin >> x;
+//
+//	answer = (x == 0) ? y = 0 : y = 10 / x;
+//	system("pause");
+//	return 0;
+//	cout << answer << endl;*/
+//	
+//	Example6
+//
+//	/*int A;
+//	int B;
+//	char op;
+//	int answer;
+//	
+//	cin >> A >> op >> B;
+//	
+//	switch (op)
+//	{
+//	case '+':
+//		answer = A + B;
+//		cout << answer << endl;
+//		break;
+//	case '-':
+//		answer = A - B;
+//		cout << answer << endl;
+//		break;
+//	case '/':
+//		answer = A / B;
+//		cout << answer << endl;
+//		break;
+//	case '*':
+//		answer = A * B;
+//		cout << answer << endl;
+//		break;
+//	case '%':
+//		answer = A % B;
+//		cout << answer << endl;
+//		break;
+//	default:
+//		cout << "invalid input" << endl;
+//	}
+//	
+//	system("pause");
+//	return 0;*/
+//
+//Example 7
 //int Month;
 //
 //for(Month = 1; Month < 13; Month++)
@@ -212,4 +219,45 @@ int main()
 //
 //system("pause");
 //return 0;
+//
+//Example 8
+//
+//bool flag = true;
+//int numPos = 35;
+//int numNeg = -55;
+//char firstChar = 'J';
+//char secondChar = 'O';
+//double frstPrice = 5.60;
+//
+//if (numPos > numNeg == flag)
+//cout << numPos << endl << numNeg << endl;
+//else
+//cout << !flag << endl;
+//
+//if (firstChar > secondChar == flag)
+//cout << firstChar << endl << secondChar << endl;
+//else
+//cout << !flag << endl;
+//
+//if (!(numPos + numNeg) == flag)
+//cout << flag << endl;
+//else
+//cout << !flag << endl;
+//
+//if (frstPrice >= 4.1 && frstPrice <= 9.9)
+//cout << flag << endl;
+//else
+//cout << !flag << endl;
+//
+//if (!flag && secondChar <= 'R')
+//cout << !flag << endl;
+//else
+//cout << flag << endl;
+//
+//
+//
+//system("pause");
+//return 0;
+//Example 9
+
 }
